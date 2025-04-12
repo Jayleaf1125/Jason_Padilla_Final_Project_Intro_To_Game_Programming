016c041cfd323db8807f739c9328f586ce977be5
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-04-12 11:50:17 -0400

    Card Placement System Done

---
+++ Assets/Scripts/CardPlacementSystem.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlacementSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int rows;
+    public int columns;
+    public GameObject cardPrefab;
+
+    public GameObject[,] cardsInPlay;
+    public float spacing;
+
+    void Start()
+    {
+        cardsInPlay = new GameObject[rows, columns];
+        CardPlacementSetUp(cardsInPlay);
+    }
+
+    GameObject[,] CardPlacementSetUp(GameObject[,] cards)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                Vector3 rightSpacing = Vector3.right * (i * spacing);
+                Vector3 downSpacing = Vector3.down * (j * spacing);
+
+                cards[i,j] = Instantiate(cardPrefab, transform.position + rightSpacing + downSpacing, Quaternion.identity);
+            }
+        }
+
+        return cards;
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/RevealCards.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RevealCards : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    SpriteRenderer _sr;
+    void Start()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
